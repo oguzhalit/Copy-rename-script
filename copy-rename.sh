61fb4c6db61d62d41d0f5575aedc9b0f0c86e33e
Oğuz <37263350+oguzhalit@users.noreply.github.com>
2019-04-14 17:56:20 +0300

    Add files via upload

---
+++ copy-rename.sh
@@ -0,0 +1,15 @@
+﻿basla=1
+for dosya in $2/*$3; do
+dosyadi=$4
+uzantisi="${dosya##*.}"
+if [ "$1" == 'directory' ]; then
+  yenidosyadi=$(printf "$dosyadi"_"$basla")
+  cp -R -- "$dosya" "$yenidosyadi"
+else 
+  yenidosyadi=$(printf "$dosyadi"_"$basla"."$uzantisi")
+  cp  -- "$dosya" "$yenidosyadi"
+fi
+
+let basla=basla+1
+
+done
